7638500f967fd91271f72aeadfa3b284e591d919
Antony Zhou <AntonyZhou@ultimatesoftware.com>
2019-05-22 16:50:05 -0400

    Can now reset scene using keyboard R key

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(Button))]
+//[RequireComponent(typeof(Button))]
 public class Restart : MonoBehaviour
 {
 
@@ -11,8 +11,14 @@ public class Restart : MonoBehaviour
 
     void Start()
     {
-        button = GetComponent<Button>();
-        button.onClick.AddListener(TaskOnClick);
+        //button = GetComponent<Button>();
+        //button.onClick.AddListener(TaskOnClick);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            TaskOnClick();
     }
 
     void TaskOnClick()
