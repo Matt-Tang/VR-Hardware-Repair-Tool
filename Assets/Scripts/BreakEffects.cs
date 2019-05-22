abc1eb2773bb1c0d266554b604edac67917fa935
Alex Qian <alex_qian@ultimatesoftware.com>
2019-05-22 11:20:20 -0400

    break effects script

---
+++ Assets/Scripts/BreakEffects.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakEffects : MonoBehaviour
+{
+    AudioSource audioSource;
+    public bool explode;
+    public float breakMagnitude;
+    public float explosionMagnitude;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > breakMagnitude)
+        {
+            if (!audioSource.isPlaying) { 
+                audioSource.Play();
+                Debug.Log("Magnitude: " + collision.relativeVelocity.magnitude);
+            }
+
+            if (explode && collision.relativeVelocity.magnitude > explosionMagnitude)
+            {
+                ParticleSystem ps = GetComponent<ParticleSystem>();
+                if (ps != null)
+                {
+                    ps.Play(true);
+                }
+            }
+        }
+    }
+}
