b95cab3267672985108b36904749fdf214ba2b95
Alex Qian <alex_qian@ultimatesoftware.com>
2019-05-22 12:18:21 -0400

    Moved restart script for cherrypicking

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class Restart : MonoBehaviour
+{
+
+    private Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+
+    }
+}
+
+//public class Restart : MonoBehaviour
+//{
+
+//    public void RestartGame()
+//    {
+//        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+//    }
+
+//}
